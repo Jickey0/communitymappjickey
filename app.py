69369a88ceac4d454b2f4a51f02a5e35f99bbdf6
Linda Geer <lindageer@Lindas-MacBook-Air-2.local>
2023-11-11 21:32:09 -0500

    added live.html, updated flask and css

--- app.py
+++ app.py
@@ -3,10 +3,12 @@ from flask import Flask, render_template, request, flash
 # initialize app
 app = Flask(__name__)
 
-app.secret_key = 'secret_key'
-
 # renders the default page
 @app.route("/")
 def index():
     markers = [-75.480858, 40.632950]
     return render_template("index.html", markers=markers)
+
+@app.route("/live")
+def live():
+    return render_template("live.html")
